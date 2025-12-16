0bcc405afbe826267f458ac2276926a8aeb99122
Lexbatol <lexbatol@gmail.com>
2025-12-17 00:49:18 +0100

    feat: dark mode with local storage persistence

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+// 1. SELECT elements
 const themeBtn = document.querySelector("#theme-toggle");
 const body = document.querySelector("body");
 
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+  body.classList.add("active-mode");
+  themeBtn.textContent = "🌙";
+}
+// 2. LISTEN for clicks
 themeBtn.addEventListener("click", function () {
-  // Toggle the class
+  // 3. CHANGE the class
   body.classList.toggle("active-mode");
 
-  // Change the icon based on the mode
+  // 4. CHANGE the icon based on the mode
   if (body.classList.contains("active-mode")) {
     themeBtn.textContent = "🌙";
+    localStorage.setItem("theme", "light");
   } else {
     themeBtn.textContent = "☀️";
+    localStorage.setItem("theme", "dark");
   }
 });
